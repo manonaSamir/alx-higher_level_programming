5ed0a20aa19baa27dfb200c7b2b0b5cda179bfcc
manonaSamir <bena.yalla@yahoo.com>
2023-11-27 22:03:39 +0200

    8. Create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-words = str.split()
-index = words.index("object-oriented")
-str = str = " ".join(words[index:index+3])
+str = str.split()[5] , str.split()[6] , str.split()[12] , str.split()[0]
+str = " ".join(str)
 print(str)
