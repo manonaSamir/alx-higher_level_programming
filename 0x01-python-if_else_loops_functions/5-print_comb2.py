b0e72ed37f884c1140a5e6a70d801ab2a45f4e50
manonaSamir <bena.yalla@yahoo.com>
2023-11-28 22:02:59 +0200

    commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for item in range(0, 99):
-    if item == 98:
+for item in range(0, 100):
+    if item == 99:
         print("{}".format(item), end="")
     else:
         print("{:02}".format(item), end=", ")
