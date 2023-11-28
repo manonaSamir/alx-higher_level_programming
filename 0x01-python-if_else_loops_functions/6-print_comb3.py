2d7014bf6936c6d3d98848d9e023afbbf491dcd9
manonaSamir <bena.yalla@yahoo.com>
2023-11-28 18:32:16 +0200

    remove one loop

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i+1, 10):
-        if i == 8 and j == 9:
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}".format(i, j), end=", ")
+for item in range(0, 100):
+    if  item == 99:
+        print("{}".format(item))
+    else:
+        print("{}".format(item), end=", ")
