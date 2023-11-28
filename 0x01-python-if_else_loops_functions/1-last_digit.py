7939b35e0b0723716783fa797d2b8ad0a3d1321c
manonaSamir <bena.yalla@yahoo.com>
2023-11-28 11:48:05 +0200

    updates negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 mod_num = abs(number) % 10
-if mod_num > 5:
+if mod_num > 5 and number > 0:
     print(f"Last digit of {number} is {mod_num} and is greater than 5")
-elif mod_num == 0:
+elif mod_num == 0 and number > 0:
     print(f"Last digit of {number} is {mod_num} and is 0")
 else:
     print(f"Last digit of {number} is {mod_num} and is less than 6 and not 0")
