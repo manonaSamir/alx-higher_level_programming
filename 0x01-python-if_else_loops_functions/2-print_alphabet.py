9f343d77cc657e17b5b2a3ed2bf826dba3f64efd
manonaSamir <bena.yalla@yahoo.com>
2023-11-28 13:23:55 +0200

    print char

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = ''.join([chr(item) for item in range(97, 123)])
-print(str)
+for item in range(97, 123):
+    print("{}".format(chr(item)), end="")
 # print(string.ascii_lowercase)
