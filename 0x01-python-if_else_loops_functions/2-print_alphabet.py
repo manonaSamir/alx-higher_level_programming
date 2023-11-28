8a183fda56685aef2db0a52e417ed780ca379b58
manonaSamir <bena.yalla@yahoo.com>
2023-11-28 13:26:45 +0200

    changes

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-print(''.join([chr(item) for item in range(97, 123)]))
-
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
+# print(string.ascii_lowercase)
