55d7b7d848947b28f3ba34e1629ad5b3ba7655ef
manonaSamir <bena.yalla@yahoo.com>
2023-11-28 21:57:13 +0200

    changes tas 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for item in range(0, 99):
-    if item <= 0:
+    if item <= 9:
         print("0{}, ".format(item), end="")
     else:
         print("{}, ".format(item), end="")
