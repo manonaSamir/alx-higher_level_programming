e52404a14cf281c5359fd695905ac8be8f020cbf
manonaSamir <bena.yalla@yahoo.com>
2023-11-30 11:54:17 +0200

    message refactor

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,11 @@ import sys
 if __name__ == "__main__":
     for i, arg in enumerate(sys.argv):
         if i == 0:
-            print(f"{len(sys.argv) - 1}", "argument." if (len(sys.argv) - 1)
-                  == 1 else "arguments:")
+            if len(sys.argv) - 1 == 1:
+                print(f"{len(sys.argv) - 1}", "argument:")
+            elif (len(sys.argv) - 1) == 0:
+                print(f"{len(sys.argv) - 1}", "arguments.")
+            else:
+                print(f"{len(sys.argv) - 1}", "arguments:")
         else:
             print(f"{i}: {arg}")
