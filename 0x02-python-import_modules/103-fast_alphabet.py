1afddf5b50145290b0336d42ff87edbb081ef4f7
manonaSamir <bena.yalla@yahoo.com>
2023-11-30 14:08:05 +0200

    upper func

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    print(*map(chr, range(65, 91)), sep='')
+import upper_alph
---
+++ 0x02-python-import_modules/upper_alph.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for item in range(65, 90):
+    print("{}".format(chr(item)), end="")
