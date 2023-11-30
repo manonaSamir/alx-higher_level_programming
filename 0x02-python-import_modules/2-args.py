74e53364e0c9d07291040c8fcb6e6fc8a67861b3
manonaSamir <bena.yalla@yahoo.com>
2023-11-30 11:45:22 +0200

    add dots

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == "__main__":
     for i, arg in enumerate(sys.argv):
         if i == 0:
-            print(f"{len(sys.argv) - 1}", "argument:" if (len(sys.argv) - 1)
+            print(f"{len(sys.argv) - 1}", "argument." if (len(sys.argv) - 1)
                   == 1 else "arguments:")
         else:
             print(f"{i}: {arg}")
