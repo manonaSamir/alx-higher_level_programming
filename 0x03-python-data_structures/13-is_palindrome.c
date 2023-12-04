5c42848ed8904c1f5a563c6734cf851269aac82f
manonaSamir <bena.yalla@yahoo.com>
2023-12-04 13:05:14 +0200

    chenges checks

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,52 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
- *
- * Return: pointer to the first node in the new list
+ * reverseList - reverse list
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: reversed list
  */
-void reverse_listint(listint_t **head)
+listint_t *reverseList(listint_t *head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
+	listint_t *prev = 0;
+	listint_t *current = head;
+	listint_t *next;
 
-	while (current)
+	while (current != 0)
 	{
 		next = current->next;
 		current->next = prev;
 		prev = current;
 		current = next;
 	}
-
-	*head = prev;
+	return (prev);
 }
 
 /**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
- *
- * Return: 1 if it is, 0 if not
+ * is_palindrome - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: if success is_palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+	listint_t *front = *head;
+	listint_t *rare = *head;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-
-	while (1)
+	while (front != NULL && front->next != NULL)
 	{
-		fast = fast->next->next;
-		if (!fast)
-		{
-			dup = slow->next;
-			break;
-		}
-		if (!fast->next)
-		{
-			dup = slow->next->next;
-			break;
-		}
-		slow = slow->next;
+		front = front->next->next;
+		rare = rare->next;
 	}
+	listint_t *reversedSecondHalf = reverseList(rare);
+	listint_t *firstHalf = *head;
 
-	reverse_listint(&dup);
-
-	while (dup && temp)
+	while (reversedSecondHalf != NULL)
 	{
-		if (temp->n == dup->n)
-		{
-			dup = dup->next;
-			temp = temp->next;
-		}
-		else
+		if (firstHalf->n != reversedSecondHalf->n)
 			return (0);
+		firstHalf = firstHalf->next;
+		reversedSecondHalf = reversedSecondHalf->next;
 	}
-
-	if (!dup)
-		return (1);
-
-	return (0);
+	return (1);
 }
