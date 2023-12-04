6422a2005d52d5c4108c7729c6595d05444b4d6a
manonaSamir <bena.yalla@yahoo.com>
2023-12-04 14:49:26 +0200

    betty changes

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -5,16 +5,16 @@
  * print_python_list_info - prints info about a python list
  * @p: list object
  */
-void print_python_list_info(PyObject* p)
+void print_python_list_info(PyObject *p)
 {
 	int i;
-	PyListObject* pp;
+	PyListObject *pp;
 	/*
 	 * lists in python are of the type PyListObject. PyObject is just a type
 	 * used to pass other types around. So you will need to cast your object back
 	 * to its oringinal type
 	 */
-	pp = (PyListObject*)p;
+	pp = (PyListObject *)p;
 
 	printf("[*] Size of the Python List = %ld\n", pp->ob_base.ob_size);
 	printf("[*] Allocated = %ld\n", pp->allocated);
@@ -22,6 +22,5 @@ void print_python_list_info(PyObject* p)
 	for (i = 0; i < pp->ob_base.ob_size; i++)
 	{
 		printf("Element %d: %s\n", i, pp->ob_item[i]->ob_type->tp_name);
-
 	}
 }
