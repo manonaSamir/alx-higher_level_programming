9b4adf5b246924b2ee700f6a80f09a9584a66786
manonaSamir <bena.yalla@yahoo.com>
2023-12-06 14:46:44 +0200

    modify code to delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    return {x: value for x, value in a_dictionary.items() if x != key}
+    a_dictionary.pop(key, None)
+    return a_dictionary
