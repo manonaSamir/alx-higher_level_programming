c7a89fa7397b365f295d97875eca276f0b8ca737
manonaSamir <bena.yalla@yahoo.com>
2023-12-06 11:37:41 +0200

    modify to accept string

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    row = [x if x != 2 else 98 for x in my_list]
+    row = [x if x != search else replace for x in my_list]
     return row
