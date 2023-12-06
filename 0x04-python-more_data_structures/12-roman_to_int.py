5c6994758dae4b610c351c3d6f658f207b6bcb59
manonaSamir <bena.yalla@yahoo.com>
2023-12-06 19:54:39 +0200

    split line

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,8 @@ def roman_to_int(roman_string):
                  "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
     for index, letter in enumerate(roman_string):
-        if len(roman_string) - 1 > index and roman_val[letter] < roman_val[roman_string[index + 1]]:
+        is_more = len(roman_string) - 1 > index
+        if is_more and roman_val[letter] < roman_val[roman_string[index + 1]]:
             total -= roman_val[letter]
         else:
             total += roman_val[letter]
