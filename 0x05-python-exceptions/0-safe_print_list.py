02640dd4ad42c1efd09de9fa5bda2393063cc50f
manonaSamir <bena.yalla@yahoo.com>
2023-12-18 11:13:01 +0200

    changes for code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        row = [item for item in my_list if item[1] <= x]
-        return(row)
-    except:
-        return("enter a valid number")
-        
+        result = [item for item in enumerate(my_list) if item[1] < x]
+        return (result)
+    except ValueError:
+        return ("enter a valid number")
