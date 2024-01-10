ab95a3d0babb840514728a8caf0e6298b1261e7d
manonaSamir <bena.yalla@yahoo.com>
2024-01-10 20:47:36 +0200

    add function

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-
 """
 "3-say_my-name" module.
-The 3-say_my_name  module supplies one function, say_my_name.
+supplies one function, say_my_name(first_name, last_name="").
 """
 
 
