579938912eac47fb91aee77749b5500b6774627b
manonaSamir <bena.yalla@yahoo.com>
2024-01-12 11:52:30 +0200

    pycode style fixing

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,11 +8,10 @@ if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
-    
     args = sys.argv[1::]
     try:
         jsonList = load_from_json_file("add_item.json")
-    except:
+    except FileNotFoundError:
         """handling the specific exception"""
         jsonList = []
 
