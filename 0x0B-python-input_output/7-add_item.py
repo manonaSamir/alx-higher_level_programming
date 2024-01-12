143bb02e476a0719eeaeca875031e2ad9d6135ce
manonaSamir <bena.yalla@yahoo.com>
2024-01-12 11:19:14 +0200

    d

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,6 +5,9 @@ import sys
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
+
+
+
 """actually does it"""
 try:
     items = load_from_json_file("add_item.json")
