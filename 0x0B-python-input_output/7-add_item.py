33879c52d790a24df54a1f0f4621741dc0928232
manonaSamir <bena.yalla@yahoo.com>
2024-01-12 11:48:44 +0200

    checker

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,13 +8,14 @@ if __name__ == "__main__":
     save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('8-load_from_json_file').load_from_json_file
-
+    
+    args = sys.argv[1::]
     try:
         jsonList = load_from_json_file("add_item.json")
     except:
         """handling the specific exception"""
         jsonList = []
 
-    for i in sys.argv[1:]:
-        jsonList.append(i)
-    save_to_json_file(jsonList, "add_item.json")
+    else:
+        jsonList += args
+        save_to_json_file(jsonList, "add_item.json")
