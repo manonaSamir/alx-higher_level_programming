d79014f7024b1b436c9b315e6a82a0a87866aeea
manonaSamir <bena.yalla@yahoo.com>
2024-01-12 13:00:14 +0200

    pycode style

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,18 +3,11 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-	'''inserts a line of text to a file, after each line containing a specific string '''
-	read = []
-	with open(filename, "r", encoding="utf-8") as f:
-		read = f.readlines()
-		index = 0
-
-		while index < len(read):
-			if search_string in read[index]:
-				read[index:index + 1] = [read[index], new_string]
-				index ++1
-			index ++1
-
-	with open(filename, "w", encoding="utf-8") as file:
-		file.writelines(read)
-		
+    """inserts a line of text to a file, after each line containing a specific string"""
+    with open(filename, mode='r', encoding='utf-8') as f:
+        lines = f.readlines()
+    with open(filename, mode='w', encoding='utf-8') as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
