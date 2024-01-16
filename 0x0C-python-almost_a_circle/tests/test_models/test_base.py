b9c2f3e5eb879acb159c87dcca95408a2bda2d5d
manonaSamir <bena.yalla@yahoo.com>
2024-01-16 17:44:54 +0200

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -84,7 +84,20 @@ class TestBase(unittest.TestCase):
         Square.save_to_file([])
         with open("Square.json", "r") as file:
             self.assertEqual(file.read(), "[]")
-            
+         
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
+        
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r2])
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(len(file.read()), 52)
+
     def test_fromJson(self):
         """tests the fromjson"""
         r_input = [{'id': 89, 'width': 10, 'height': 4}]
