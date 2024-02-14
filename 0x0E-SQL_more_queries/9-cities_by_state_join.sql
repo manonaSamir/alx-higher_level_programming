287aa63a57fca7b8cbacd6629eca3ab64a51d879
manonaSamir <bena.yalla@yahoo.com>
2024-02-14 03:42:56 -0800

    sandbox

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
---lists all cities contained in the database hbtn_0d_usa
+-- lists all cities contained in the database hbtn_0d_usa
 SELECT 	c.id, c.name, s.name FROM cities AS c INNER JOIN states AS s ON c.state_id = s.id ORDER c.id;
