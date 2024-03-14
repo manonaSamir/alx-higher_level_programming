a32778228f2ff81b705f251b1f764aaa7efb23c0
manonaSamir <bena.yalla@yahoo.com>
2024-03-14 23:00:19 +0200

    test sql

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""a script that lists all states from the database hbtn_0e_0_usa"""
+
+from MySQLdb import _mysql
+import sys
+
+if __name__ == "__main__":
+    db=_mysql.connect()
+    args = sys.argv
+    username = args[1]
+    password = args[2]
+    db_name = args[3]
+    db=_mysql.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=db_name, charset="utf8")
+    db.query("""SELECT * FROM states ORDER BY id""")
+    rows = db.store_result()
+    for row in rows:
+        print(f"({row[0]}, '{row[1]}')")
