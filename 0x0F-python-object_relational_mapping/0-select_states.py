866448eedcdfb53cbbc2c34589d542feac6ba58e
manonaSamir <bena.yalla@yahoo.com>
2024-03-14 23:58:13 +0200

    fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     dbConnect = _mysql.connect(host="localhost", port = 3306, user = username, password= password, database = db_name, charset="utf8")    
     dbConnect.query("""SELECT * FROM states ORDER BY id""")
     result = dbConnect.use_result()
-    rows = result.fetchall()   
-    for row in rows:
+    row = result.fetch_row()
+    while row:
         print(f"({row[0]}, '{row[1]}')")
+        row = result.fetch_row()
