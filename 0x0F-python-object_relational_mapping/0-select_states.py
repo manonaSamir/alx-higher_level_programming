79fe55b7f46132a12b1c426af1f2a9d21eeb62af
manonaSamir <bena.yalla@yahoo.com>
2024-03-15 00:24:44 +0200

    delete dab

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port = 3306, user = username, password= password, database = db_name, charset="utf8")
     
     result = db.cursor()
-    result.execute("""SELECT * FROM states ORDER BY id""")  
+    # result.execute("""SELECT * FROM states ORDER BY id""")  
+    result.execute("""DROP DATABASE IF EXISTS hbtn_0e_0_usa""")  
     rows = result.fetchall()
     for row in rows:
        print(row)
     result.close()
-        
