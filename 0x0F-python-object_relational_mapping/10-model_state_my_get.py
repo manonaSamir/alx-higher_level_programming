7b41cb38856e4843f23cacd47da8995c6f40ffe2
manonaSamir <bena.yalla@yahoo.com>
2024-03-15 23:16:49 +0200

    fix code style

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database """
+""" prints the State with the name passed from the database """
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
