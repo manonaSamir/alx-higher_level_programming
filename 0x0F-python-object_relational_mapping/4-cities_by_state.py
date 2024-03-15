b0685acfc55642ad3ff175f54a18b27aacd6eb2b
manonaSamir <bena.yalla@yahoo.com>
2024-03-15 11:24:42 -0700

    sandbox

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    name = sys.argv[4]
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          password=password, database=db_name, charset="utf8")
     result = db.cursor()
