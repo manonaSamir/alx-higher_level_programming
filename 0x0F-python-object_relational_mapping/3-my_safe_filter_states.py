8ac17d35557c37a9593a34d803e880782ab7e0d9
manonaSamir <bena.yalla@yahoo.com>
2024-03-15 20:16:36 +0200

    write one that is safe from MySQL injections!

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" displays values in the states table where name matches the argument. """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    name = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         password=password, database=db_name, charset="utf8")
+    result = db.cursor()
+    result.execute("SELECT * FROM states WHERE name LIKE %s", (name, ))
+    rows = result.fetchall()
+    for row in rows:
+        print(row)
+    result.close()
