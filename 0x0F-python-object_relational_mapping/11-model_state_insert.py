2cfd76332b9d8e107cfbebf92127ccc2c4a288ca
manonaSamir <bena.yalla@yahoo.com>
2024-03-15 23:33:58 +0200

    v

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, db_name))
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
