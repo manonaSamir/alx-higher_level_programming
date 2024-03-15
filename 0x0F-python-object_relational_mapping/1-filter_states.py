8d163a5ab163a7a755e221dae5eaaa14d838b9a9
manonaSamir <bena.yalla@yahoo.com>
2024-03-15 14:25:27 +0200

    filter by name

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""a script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         password=password, database=db_name, charset="utf8")
+    result = db.cursor()
+    result.execute("""SELECT * FROM states WHERE name
+                LIKE 'N%' ORDER BY states.id""")
+    rows = result.fetchall()
+    for row in rows:
+        print(row)
+    result.close()
