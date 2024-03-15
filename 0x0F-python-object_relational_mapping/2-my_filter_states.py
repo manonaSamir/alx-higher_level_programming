c1f951410a34479a1ef7eff4345be36bb576b319
manonaSamir <bena.yalla@yahoo.com>
2024-03-15 14:47:19 +0200

    Search by Name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" lists all states with a name starting with N from hbtn_0e_0_usa """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    name = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         password=password, database=db_name, charset="utf8")
+    result = db.cursor()
+    result.execute("""SELECT * FROM states WHERE LIKE BINARY '{}'"
+                .format(name))""")
+    rows = result.fetchall()
+    for row in rows:
+        print(row)
+    result.close()
