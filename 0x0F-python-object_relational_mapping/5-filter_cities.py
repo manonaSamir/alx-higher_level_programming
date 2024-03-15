549d7acc56b2b9c851c8b261b3d807a2628f8bfa
manonaSamir <bena.yalla@yahoo.com>
2024-03-15 22:07:16 +0200

    change show

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
                 cities INNER JOIN states ON states.id=cities.state_id
                 WHERE states.name=%s""", (name,))
     rows = result.fetchall()
-    for row in rows:
-        print(row)
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
     result.close()
