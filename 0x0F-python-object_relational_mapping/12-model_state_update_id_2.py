1438e7c0971d6324a69561fdba6bb5f98164ee5b
manonaSamir <bena.yalla@yahoo.com>
2024-03-16 16:19:43 +0200

    s

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(username, password, db_name),
                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
@@ -20,4 +20,4 @@ if __name__ == "__main__":
     if new_instance:
         new_instance.name = 'New Mexico'
         session.commit()
-        session.close()
+
