c7706c2cc3e546d3ade8b2e1d5ac357311a1af93
manonaSamir <bena.yalla@yahoo.com>
2024-03-16 16:18:05 +0200

    a

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """ Module changes the name of a State object from the database """
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, db_name),
                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
