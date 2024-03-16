894d9a580cbf9321dee70f38aed252111f4b01f8
manonaSamir <bena.yalla@yahoo.com>
2024-03-16 19:29:37 +0200

    ffffff

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 # Model city
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base
 from model_state import Base, State
 
 
 class City(Base):
-    # creates city
+    """ State class that inhertis from base."""
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'),
-                      nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
