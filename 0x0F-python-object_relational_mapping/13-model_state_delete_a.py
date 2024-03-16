247918ee09f9e3caf0fd67f0a828887a4117cae0
manonaSamir <bena.yalla@yahoo.com>
2024-03-16 14:25:14 +0200

    styling code

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(username, password, db_name))
     Base.metadata.create_all(engine)
