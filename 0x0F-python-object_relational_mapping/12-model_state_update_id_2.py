dd58fcbb4e879facdee9b8f7fccf9f755e2596a3
manonaSamir <bena.yalla@yahoo.com>
2024-03-16 14:39:04 +0200

    changres

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
 """ changes the name of a State object from the database """
-import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
-from sqlalchemy.orm import Session
-from sqlalchemy import select
-
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, db_name))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = Session(engine)
-    query = select(State).where(State.id == 2)
-    state = session.execute(query).fetchone().State
-    state.name = 'New Mexico'
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    mexico = session.query(State).filter(
+        State.id == 2).first()
+    mexico.name = "New Mexico"
     session.commit()
     session.close()
-
