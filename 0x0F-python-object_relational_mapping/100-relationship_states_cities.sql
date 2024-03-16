3f176b6fe0346d7bfe032834faae7f78dcb793fc
manonaSamir <bena.yalla@yahoo.com>
2024-03-16 11:48:21 -0700

    sandbox

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.sql
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.sql
@@ -4,7 +4,3 @@ USE hbtn_0e_100_usa;
 
 SELECT * FROM states;
 SELECT * FROM cities;
-
-guillaume@ubuntu:~/0x0F$ cat 100-relationship_states_cities.sql | mysql -uroot -p
-Enter password: 
-ERROR 1146 (42S02) at line 5: Table 'hbtn_0e_100_usa.states' doesn't exist
