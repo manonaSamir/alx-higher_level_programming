5431aa4eba265375e6311aabbb50ca6366acef72
manonaSamir <bena.yalla@yahoo.com>
2024-03-28 23:25:55 +0200

    style cide

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,5 +8,5 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
-    request= requests.post(url, data=value)
+    request = requests.post(url, data=value)
     print(request.text)
