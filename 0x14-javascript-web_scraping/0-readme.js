2b32d2250f779ff783969f3166bbb477f46dcddc
manonaSamir <bena.yalla@yahoo.com>
2024-04-23 19:28:23 +0200

    test

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,6 @@
 
 const file = process.argv[2];
 const fs = require('fs');
-fs.readFile(file, 'utf8', function (err, data = err) {
+fs.readFile(file, 'utf8', (err, data = err) => {
   console.log(data);
 });
