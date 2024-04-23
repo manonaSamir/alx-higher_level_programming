fb79d10d2d4fc43be9de6d59c35f747f8e10b2c4
manonaSamir <bena.yalla@yahoo.com>
2024-04-23 20:16:14 +0200

    return myfile

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
 
-const fs = require('fs');
 const file = process.argv[2];
-
-function readPrint (file) {
-  fs.readFile(file, 'utf-8', function (err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(data);
-    }
-  });
-}
-readPrint(file);
+const fs = require('fs');
+fs.readFile(file, 'utf8', (err, data = err) => {
+  console.log(data);
+});
--- 0x14-javascript-web_scraping/0-readme1.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const file = process.argv[2];
-const fs = require('fs');
-fs.readFile(file, 'utf8', (err, data = err) => {
-  console.log(data);
-});
