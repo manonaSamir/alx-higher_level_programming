6839359eeffad8bb9994d18d5d766e60858628f3
manonaSamir <bena.yalla@yahoo.com>
2024-04-23 19:38:34 +0200

    request

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const address = process.argv[2];
+request(address, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const fs = require('fs');
+    fs.writeFile(process.argv[3], body, function (err) {
+      if (err) { console.log(err); }
+    });
+  }
+});
